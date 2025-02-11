ade65a1874c5a2a95dc0a77054506d266b91d124
Samuel <samuelpremiun11@gmail.com>
2025-02-11 20:50:48 +0100

    Update code and file .csv

---
+++ Control_De_Parametros.py
@@ -0,0 +1,65 @@
+import random
+import time
+from twilio.rest import Client
+
+#PARÁMETROS DE SERVICIO A RELLENAR
+TWILIO_SID = 'AC415cbf150e279c04d245f7ce7ed0361a'
+TWILIO_AUTH_TOKEN = 'd129c4974d97de9fe815fa6292c43c21'
+TWILIO_TELEFONO = '+12313887434'
+MOVIL_TRABAJADORES = ['+34679221363']  #TELÉFONOS DE AVISO DE TRABAJADORES
+
+
+
+def enviar_sms(parametro, valor):
+    """Envía un SMS a los trabajadores si hay una alerta."""
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    for phone in MOVIL_TRABAJADORES:
+        mensaje = f"[ALERTA] {parametro} fuera de rango: {valor:.1f}"
+        client.messages.create(body=mensaje, from_=TWILIO_TELEFONO, to=phone)
+        print(f"SMS enviado a {phone}: {mensaje}")
+
+
+
+#LÍMITES DE ALERTA
+TEMP_LIMITE = 30.0  # Temperatura máxima en °C
+HUM_LIMITE = 70.0   # Humedad máxima en %
+
+
+def registrar_datos(temp, hum):
+    """Registra los datos en un archivo."""
+    with open("datos_veterinario.csv", "a") as archivo:
+        archivo.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')},{temp:.1f},{hum:.1f}\n")
+
+def monitorear():
+    """Simula la lectura y monitoreo de sensores."""
+    while True:
+
+        # Simular datos de sensores
+        #CON ESTO GENERAMOS UNA SERIE DE DATOS DE TEMPERATRURA Y SENSORES QUE DEBERÍAN DE PROPORCIONAR SENSORES EN CASO REAL
+
+        #temperatura = random.uniform(20.0, 40.0)
+        #humedad = random.uniform(40.0, 90.0)
+
+        temperatura = 35.7 
+        humedad = 68  
+
+        print(f"Temperatura: {temperatura:.1f}°C, Humedad: {humedad:.1f}%")
+        registrar_datos(temperatura, humedad)
+
+        #CUANDO LLAMAMOS A LA FUNCIÓN DE registrar_datos, SE ABRE EL FICHERO .CSV DONDE SE ALMACENAN ESOS DATOS
+
+        # Verificar umbrales
+        if temperatura > TEMP_LIMITE:
+            enviar_sms("Temperatura", temperatura)
+        if humedad > HUM_LIMITE:
+            enviar_sms("Humedad", humedad)
+
+        return 1
+
+        time.sleep(2)  # Pausa de 5 segundos entre lecturas
+
+if __name__ == "__main__":
+    try:
+        monitorear()
+    except KeyboardInterrupt:
+        print("\nMonitoreo detenido.")
